dcfce8cb8d4936b9876e739889e7181c2915e443
ppp16bit <pcampossobrinho@gmail.com>
2025-03-25 21:49:37 -0400

    lexer header

---
+++ lexer.h
@@ -0,0 +1,20 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+typedef enum {
+    TOKEN_INT, TOKEN_IDENT, TOKEN_KEYWORD, TOKEN_PLUS, TOKEN_MINUS,
+    TOKEN_MUL, TOKEN_DIV, TOKEN_LPAREN, TOKEN_RPAREN, TOKEN_COLON, 
+    TOKEN_EQ, TOKEN_IF, TOKEN_WHILE, TOKEN_EOF, TOKEN_NEWLINE,
+    TOKEN_LT, TOKEN_GT, TOKEN_LE, TOKEN_GE, TOKEN_NE, TOKEN_EE
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char *value;
+    int ivalue;
+} Token;
+
+void lexer_init(char *input);
+Token lexer_next_token();
+
+#endif
